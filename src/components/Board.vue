e95b635de8a470fce229828c7490b870e73017d3
uni51 <uni51@users.noreply.github.com>
2020-07-20 13:38:12 +0900

    3-1 ListAddコンポーネントを作成する

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <header>
-      my Trello
-    </header>
+    <header>my Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <list-add />
     </main>
   </div>
 </template>
 
 <script>
+import ListAdd from "./ListAdd.vue";
+
 export default {
-}
+  components: {
+    ListAdd
+  }
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,55 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+// localStorageに保存したリストを取得する
+const savedLists = localStorage.getItem("trello-lists");
+
+const store = new Vuex.Store({
   state: {
+    // localStorageに保存されたリストがあれば取得、なければデフォルトのリスト配列を設置する
+    // localStorageにはJSON形式の文字列型でデータが保存されているので、
+    // 取得するときにはJSON.parse(取得するデータ)でオブジェクトに変換する必要があ
+    lists: savedLists
+      ? JSON.parse(savedLists)
+      : [
+          {
+            title: "Backlog",
+            cards: [{ body: "English" }, { body: "Mathematics" }],
+          },
+          {
+            title: "Todo",
+            cards: [{ body: "Science" }],
+          },
+          {
+            title: "Doing",
+            cards: [],
+          },
+        ],
   },
   mutations: {
+    // mutationsはあくまでもストアの状態の変更だけを行う
+    // mutationsの重要なルールとして、同期的でなければならないというのがある
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards: [] });
+    },
   },
   actions: {
+    // mutationsの呼び出しはactionsが行う
+    // 第一引数にcontextというストアインスタンスのメソッドやプロパティを呼び出せるオブジェクトを受け取ることができる
+    addlist(context, payload) {
+      context.commit("addlist", payload);
+    },
   },
-  modules: {
-  }
-})
+  getters: {},
+});
+
+// データの状態を更新後にlocalStorageへデータの状態を保存する
+// subscribeはストアのインスタンスメソッドで、全てのmutationの後に呼ばれる
+store.subscribe((mutation, state) => {
+  // 保存するときは任意のキーを設定して、データをJSON形式（文字列型）に変換して保存する
+  localStorage.setItem("trello-lists", JSON.stringify(state.lists));
+});
+
+export default store;
