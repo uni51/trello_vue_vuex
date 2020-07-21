233fa252d9d630055ad4440a8aff2a4d06b368ae
uni51 <uni51@users.noreply.github.com>
2020-07-21 16:29:17 +0900

    3-3 Listコンポーネントを作成しよう

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,17 +3,29 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
-      <list-add />
+      <div class="list-index">
+        <!-- Listコンポーネントを呼び出し、Listコンポーネントに必要なデータを渡しています -->
+        <list v-for="(item, index) in lists" :key="item.id" :title="item.title" :listIndex="index" />
+        <list-add />
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 import ListAdd from "./ListAdd.vue";
+import List from "./List";
+import { mapState } from "vuex";
 
 export default {
   components: {
-    ListAdd
+    ListAdd,
+    List
+  },
+  computed: {
+    // ストア（store/index.js）のstateで定義されたデータの名前と同じ名前の文字列でstateを呼び出すことができる
+    // computedにストアのstateからのデータと、ローカルでの算出データを両方定義する場合には、このように書く必要がある
+    ...mapState(["lists"])
   }
 };
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <div class="deletelist" v-on:click="removeList">×</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // propsには、親コンポーネントから受け取るデータを定義できる
+  // propsでデータを受け取る時は、型などの条件を指定できる
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    listIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    removeList: function() {
+      if (confirm("本当にこのリストを削除しますか？")) {
+        this.store.dispatch("removelist", { listIndex: this.listIndex });
+      }
+    }
+  }
+};
+</script>
